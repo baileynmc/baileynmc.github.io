525028f88da724837869027e6fe142ecf171b546
baileynmc <baileynmc@gmail.com>
2020-06-08 16:36:39 -0400

    Updates

---
+++ precache-manifest.df18fda877ecc1999a214b357d3d9f86.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1208a1aa12a9b8d913172899e79f1a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "491dafa7fac645ad6513",
+    "url": "/static/css/main.6c53469f.chunk.css"
+  },
+  {
+    "revision": "4df007a9e9a4b4dcbf92",
+    "url": "/static/js/2.17510fb5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.17510fb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "491dafa7fac645ad6513",
+    "url": "/static/js/main.2f54f9e7.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  },
+  {
+    "revision": "ed59e18b2bbad371ffb826d5c6d4af98",
+    "url": "/static/media/ArtPost.ed59e18b.png"
+  },
+  {
+    "revision": "c5bc2c9c44649ff646595fbd6007050f",
+    "url": "/static/media/CalenStarLogo.c5bc2c9c.png"
+  },
+  {
+    "revision": "37ff997df7273faade07ced1e8d8719b",
+    "url": "/static/media/IMG_6215.37ff997d.png"
+  },
+  {
+    "revision": "80221fe721f9041ac18c208efc6efa4c",
+    "url": "/static/media/IMG_6996.80221fe7.jpg"
+  },
+  {
+    "revision": "6460c1ed4844476b38fbf3e23d4581f9",
+    "url": "/static/media/NominationGraphicBlue.6460c1ed.png"
+  },
+  {
+    "revision": "49f99541bc2b4853579612ef4005bb06",
+    "url": "/static/media/ProfilePageSelf.49f99541.png"
+  },
+  {
+    "revision": "5a3ec7783702d99d7a69dce021c0783f",
+    "url": "/static/media/ROYALTY2x3.5a3ec778.jpg"
+  },
+  {
+    "revision": "5717b15d6bf6db6ec388c2eec9a58237",
+    "url": "/static/media/WatchList.5717b15d.png"
+  },
+  {
+    "revision": "322c4abeabbd1f5f7da0eaee02c97371",
+    "url": "/static/media/aboutMe.322c4abe.png"
+  },
+  {
+    "revision": "afe83fa3952fa702f082ff0baa699e38",
+    "url": "/static/media/anneCoverPhoto.afe83fa3.png"
+  },
+  {
+    "revision": "37f90aa8917df2dc20f06dee6a674e2b",
+    "url": "/static/media/artionLogo.37f90aa8.png"
+  },
+  {
+    "revision": "e259436e1e3d58e04f4ec5556f5a49d6",
+    "url": "/static/media/artionPrototype.e259436e.gif"
+  },
+  {
+    "revision": "5f77289701bbe4ae157242b4177fd8e0",
+    "url": "/static/media/calenstarFriends.5f772897.png"
+  },
+  {
+    "revision": "868f8934a4d68ee8ef60dd61dfabe6ea",
+    "url": "/static/media/calenstarHome.868f8934.png"
+  },
+  {
+    "revision": "7d95e537e484827f12e9d04e0eca9602",
+    "url": "/static/media/calenstarLanding.7d95e537.png"
+  },
+  {
+    "revision": "00a085e2e37941e9d04c1fb03c5c27a2",
+    "url": "/static/media/coding.00a085e2.png"
+  },
+  {
+    "revision": "82c96a52482e1b91945ee6aedd10b2ca",
+    "url": "/static/media/creativeHeadshot.82c96a52.jpg"
+  },
+  {
+    "revision": "bb01e4fb828018dd0faca8424e81bb13",
+    "url": "/static/media/festivalofnations.bb01e4fb.png"
+  },
+  {
+    "revision": "db322e59c8240937bb2dca26674d2c42",
+    "url": "/static/media/home.db322e59.png"
+  },
+  {
+    "revision": "d65a8ebd55e7c0bb4f51467efa6ab17a",
+    "url": "/static/media/janelleTideTalks30.d65a8ebd.png"
+  },
+  {
+    "revision": "4d3b4199ae48faf170b0abc836b55830",
+    "url": "/static/media/lizzoCoverPhoto.4d3b4199.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4763733fed457b58d5d72f42c1603dc7",
+    "url": "/static/media/manicure.4763733f.png"
+  },
+  {
+    "revision": "88df658e4de0fed0684d1f0919ab66c1",
+    "url": "/static/media/mastercard.88df658e.JPG"
+  },
+  {
+    "revision": "062ddb577eeb1ce56297a47c89318ea8",
+    "url": "/static/media/meghanTideTalks30.062ddb57.png"
+  },
+  {
+    "revision": "40ebb2b70e857cdb9b44e6816fd77e59",
+    "url": "/static/media/mooyahSquare.40ebb2b7.png"
+  },
+  {
+    "revision": "2b203e59a87c9faec67d53512e870205",
+    "url": "/static/media/norrisSpeakerPhoto.2b203e59.png"
+  },
+  {
+    "revision": "cc862928686a69710e014ab764f0b091",
+    "url": "/static/media/obamaTideTalks30.cc862928.png"
+  },
+  {
+    "revision": "fc88476ce9a2c2945182a5109b025c25",
+    "url": "/static/media/portfolioHome.fc88476c.png"
+  },
+  {
+    "revision": "bfb785ac218fe49ac6f60ca6634e7315",
+    "url": "/static/media/postsurvey.bfb785ac.png"
+  },
+  {
+    "revision": "d326a95b5c125a148e6efb4757713173",
+    "url": "/static/media/presurvey.d326a95b.png"
+  },
+  {
+    "revision": "5a121e8ce5081f1a8d6e165cd0307ed2",
+    "url": "/static/media/runningWaterProtoClick.5a121e8c.gif"
+  },
+  {
+    "revision": "5eb81aed5a9b3445089a80a502dc2d5e",
+    "url": "/static/media/saveTheDate.5eb81aed.png"
+  },
+  {
+    "revision": "c1c1a6646534f1091ce9e7e5af01ef14",
+    "url": "/static/media/saveTheDateFINAL.c1c1a664.png"
+  },
+  {
+    "revision": "ac1e2e368e124908c9ed29fefebf615b",
+    "url": "/static/media/statement.ac1e2e36.png"
+  },
+  {
+    "revision": "646694c0eccfc51c4afa28785dd1121c",
+    "url": "/static/media/tideTalks28Edison.646694c0.png"
+  },
+  {
+    "revision": "c2dfec60d333a2966a4a2ef8661aebdb",
+    "url": "/static/media/tideTalks28Eleanor.c2dfec60.png"
+  },
+  {
+    "revision": "788df43cd3ebb1cbd05be5f1d78578de",
+    "url": "/static/media/tideTalks28Zendaya.788df43c.png"
+  },
+  {
+    "revision": "58871200e4b638682d401424df6abf71",
+    "url": "/static/media/tideTalksRoosevelt3x2.58871200.png"
+  },
+  {
+    "revision": "d221a172356b0dc9a1a138fa5a6d9896",
+    "url": "/static/media/vanGogh3x2.d221a172.png"
+  },
+  {
+    "revision": "149d126b94b6a664552b7debfa64115c",
+    "url": "/static/media/vanGoghCoverPhoto.149d126b.png"
+  }
+]);
